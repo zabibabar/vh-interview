295b2a5e7f0d8090afec2d3c54f583beb514368a
Zabi Babar <zabeeh@tech-house.com>
2020-07-21 22:08:29 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vh-interview/precache-manifest.783c84c3b8bc7cb97fbaf7af06e080c9.js"
+  "/vh-interview/precache-manifest.e424f007c8cb88a6d12af2e79b99b1a8.js"
 );
 
 self.addEventListener('message', (event) => {
