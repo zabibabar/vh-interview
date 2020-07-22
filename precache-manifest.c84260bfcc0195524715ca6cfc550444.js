f1e39095aa12ff2153a0f7dcfd1be816eba6f38e
Zabi Babar <zabeeh@tech-house.com>
2020-07-21 21:06:45 -0400

    Updates

---
+++ precache-manifest.c84260bfcc0195524715ca6cfc550444.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4cf3da79cc8234831648c390cbb717",
+    "url": "/vh-interview/index.html"
+  },
+  {
+    "revision": "5bcf4448e978104842bc",
+    "url": "/vh-interview/static/css/main.7d3fccde.chunk.css"
+  },
+  {
+    "revision": "4bcd5970b9ba422d7165",
+    "url": "/vh-interview/static/js/2.6aedbca6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/vh-interview/static/js/2.6aedbca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcf4448e978104842bc",
+    "url": "/vh-interview/static/js/main.1a1d4d76.chunk.js"
+  },
+  {
+    "revision": "ad467c671a88a37fe802",
+    "url": "/vh-interview/static/js/runtime-main.8881b190.js"
+  }
+]);
